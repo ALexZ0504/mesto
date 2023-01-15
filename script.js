eb6803d43d93f6189be21835f4fd2abfae541920
Alex Z <sasha.zakutajlo@mail.ru>
2023-01-15 16:45:47 +0300

    Функция лайка, стили, шрифт, новый попап

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// popup редактирования профиля + отправка формы
-
+// popup редактирования профиля: открытие, закрытие и отправка формы
 const editButton = document.querySelector('.profile__edit-button');
 const nameProfile = document.querySelector('.profile__name');
 const descriptionProfile = document.querySelector('.profile__description');
@@ -22,8 +21,10 @@ function close() {
 
 function submitForm(e) {
   e.preventDefault();
+
   nameProfile.textContent = nameInput.value;
   descriptionProfile.textContent = descriptionInput.value;
+
   close();
 }
 
@@ -32,7 +33,6 @@ popupExit.addEventListener('click', close);
 form.addEventListener('submit', submitForm);
 
 // popup добавления места
-
 const popupCreate = document.querySelector('.popup-create');
 const formCreate = document.querySelector('.popup-create__container');
 const exitCreate = document.querySelector('.popup-create__exit-button');
@@ -42,19 +42,7 @@ const linkCreate = document.querySelector('.popup-create__link');
 const saveCreate = document.querySelector('.popup-create__save-button');
 const addButton = document.querySelector('.profile__add-button');
 
-function addPlace() {
-  popupCreate.style.display = "flex";
-}
-
-function closePlace() {
-  popupCreate.style.display = "none";
-}
-
-addButton.addEventListener('click', addPlace);
-exitCreate.addEventListener('click', closePlace);
-
-// template добавить в DOM и на страницу
-
+// массив исходных карточек
 const initialCards = [
   {
     name: 'Архыз',
@@ -82,15 +70,53 @@ const initialCards = [
   }
 ];
 
-initialCards.forEach(function (el) {
-  const elements = document.querySelector('.elements');
-  const template = document.querySelector('#element').content;
+// функция создания template в DOM и добавление на страницу
+const elements = document.querySelector('.elements');
+const template = document.querySelector('#element').content;
+
+function addCard(el) {
   const card = template.querySelector('.elements__element').cloneNode(true);
   const image = card.querySelector('.elements__image');
   const place = card.querySelector('.elements__place');
+
   image.src = el.link;
   image.alt = el.name;
   place.textContent = el.name;
 
   elements.append(card);
+};
+
+// метод, применяемый к каждому элементу массива
+initialCards.forEach(addCard);
+
+// popup добавления места: открыть, закрыть, отправить форму (в процессе)
+function addPlace() {
+  popupCreate.style.display = "flex";
+}
+
+function closePlace() {
+  popupCreate.style.display = "none";
+}
+
+function create(evt) {
+  evt.preventDefault();
+
+  document.querySelector('.elements__place').textContent = placeCreate.value;
+  document.querySelector('.elements__image').src = linkCreate.value;
+
+  closePlace();
+}
+
+addButton.addEventListener('click', addPlace);
+exitCreate.addEventListener('click', closePlace);
+formCreate.addEventListener('submit', create);
+
+// функция лайка
+const like = document.querySelector('.elements__like-button');
+like.addEventListener('click', function (e) {
+  if (like.className === 'elements__like-button') {
+    like.className = 'elements__like-button_active';
+  } else {
+    like.className = 'elements__like-button';
+  }
 });
